c95dc558fda5e4d257e035004993b97a776c009d
trastanechora <trastanechora@gmail.com>
2020-10-13 20:23:22 +0700

    Breakdown footer component

---
+++ components/Footer.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-footer padless>
+    <v-card flat tile width="100%" class="red lighten-1 text-center">
+      <v-card-text>
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ languageSetting.firstLineDisclaimer }} <br />
+        {{ languageSetting.secondLineDisclaimer }}
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Footer extends Vue {
+  /* ------------------------------------
+  => Local State Declaration
+  ------------------------------------ */
+  icons: string[] = [
+    'mdi-facebook',
+    'mdi-twitter',
+    'mdi-linkedin',
+    'mdi-instagram'
+  ]
+
+  /* ------------------------------------
+  => Setter and Getter
+  ** (Adopt store variables to local state)
+  ------------------------------------ */
+  get languageSetting(): boolean {
+    return this.$store.state.ui.languageSetting.footer
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.full-width {
+  max-width: none;
+}
+.no-padding {
+  padding: 0 !important;
+}
+.transform-none {
+  text-transform: none;
+  letter-spacing: inherit;
+}
+</style>
