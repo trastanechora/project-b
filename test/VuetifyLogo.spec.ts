4458880db6473b8f89773d3ac96287aeb222039e
trastanechora <trastanechora@gmail.com>
2020-10-13 20:22:00 +0700

    Delete some unused files

--- test/VuetifyLogo.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount, Wrapper } from '@vue/test-utils'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-describe('VuetifyLogo', () => {
-  test('is a Vue instance', () => {
-    const wrapper: Wrapper<Vue> = mount(VuetifyLogo)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
