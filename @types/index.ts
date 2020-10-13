0d08aac8b7e29abf092c12e2c4cc017dd0bf11f4
trastanechora <trastanechora@gmail.com>
2020-10-13 20:21:38 +0700

    Update typings

--- @types/index.ts
+++ @types/index.ts
@@ -1,2 +1,10 @@
+import { Store } from 'vuex'
+
+/* -------------------------------------------------
+=> This is necessary for direct import
+=> e.g. import { SidebarMenuItem } from './@types'
+------------------------------------------------- */
+export default Store
 export * from './ui'
 export * from './news'
+export * from './promo'
---
+++ @types/promo.ts
@@ -0,0 +1,7 @@
+export interface PromoItem {
+  color: string
+  src: string
+  title: string
+  amount: string
+  validOn: string
+}
