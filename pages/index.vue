9325f72f982d015b7dfd4622757ff04ea4760193
trastanechora <trastanechora@gmail.com>
2020-11-03 11:35:03 +0700

    Update calculation page

--- pages/index.vue
+++ pages/index.vue
@@ -1,136 +1,27 @@
 <template>
   <v-layout justify-center row wrap>
-    <v-parallax
+    <!-- <v-parallax
       class="banner"
       src="https://lajollamom.com/wp-content/uploads/2016/01/how-to-save-money-on-rental-cars.jpg"
-    ></v-parallax>
-    <!-- TRIP FORM Section -->
-    <TripInputBox />
-    <!-- ANNOUNCEMENT Section -->
-    <v-flex md12 sm12 xs12 mt-3>
-      <v-layout justify-center row wrap>
-        <v-flex xl6 md8 sm10 xs10 class="announcer">
-          <h2 id="announcement-lable">{{ languageSetting.announcements }}</h2>
-          <hr />
-          <v-layout class="slider-content">
-            <v-flex
-              v-for="(item, i) in announcements"
-              :key="i"
-              ma-3
-              class="slider-item"
-            >
-              <AnnouncementCard
-                :color="item.color"
-                :src="item.src"
-                :title="item.title"
-                :description="item.description"
-              />
-            </v-flex>
-          </v-layout>
-        </v-flex>
-      </v-layout>
-    </v-flex>
-    <!-- PROMO Section -->
-    <v-flex md12 sm12 xs12 mt-3>
-      <v-layout justify-center row wrap>
-        <v-flex xl6 md8 sm10 xs10 class="announcer">
-          <h2 id="promo-lable">{{ languageSetting.promoAndDeals }}</h2>
-          <hr />
-          <v-layout class="slider-content">
-            <v-flex v-for="(item, i) in deals" :key="i" lg4 md12 sm12 ma-3>
-              <PromoCard
-                :src="item.src"
-                :title="item.title"
-                :amount="item.amount"
-                :valid-on="item.validOn"
-              />
-            </v-flex>
-          </v-layout>
-        </v-flex>
-      </v-layout>
-    </v-flex>
+    ></v-parallax> -->
+    <div class="banner"></div>
+    <ListToBuyInputBox />
   </v-layout>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator';
-import TripInputBox from '~/components/TripInputBox.vue';
-import AnnouncementCard from '~/components/AnnouncementCard.vue';
-import PromoCard from '~/components/PromoCard.vue';
-import { NewsItem, PromoItem } from '~/@types';
+import ListToBuyInputBox from '~/components/ListToBuyInputBox.vue';
 
 @Component({
   components: {
-    TripInputBox,
-    AnnouncementCard,
-    PromoCard
+    ListToBuyInputBox
   }
 })
 export default class Home extends Vue {
   /* ------------------------------------
   => Local State Declaration
   ------------------------------------ */
-  announcements: NewsItem[] = [
-    {
-      color: '#1F7087',
-      src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
-      title: 'Download MySejahtera',
-      description:
-        'As our new policy takes place, MySejahtera apps is mandatory. Read more!'
-    },
-    {
-      color: '#952175',
-      src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-      title: 'New Fresh Interface',
-      description:
-        'As our new interface get a new paints, MySejahtera apps is getting better. Read more!'
-    },
-    {
-      color: '#952175',
-      src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-      title: 'Suitable for All Devices',
-      description:
-        'As our new innovation, MySejahtera apps is fit great in all of your devices. Read more!'
-    }
-  ];
-
-  deals: PromoItem[] = [
-    {
-      color: '#1F7087',
-      src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
-      title: 'Download App Get 50% off',
-      amount: '-50%',
-      validOn: 'October 2020'
-    },
-    {
-      color: '#952175',
-      src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-      title: 'Invite Friends get Refferals',
-      amount: '-25%',
-      validOn: 'October 2020'
-    },
-    {
-      color: '#952175',
-      src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-      title: 'One Time Deal Prize',
-      amount: '-70%',
-      validOn: 'November 2020'
-    },
-    {
-      color: '#952175',
-      src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-      title: 'Take Survey get 10% off',
-      amount: '-10%',
-      validOn: 'December 2020'
-    },
-    {
-      color: '#1F7087',
-      src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
-      title: 'New Year Free Ride!',
-      amount: '-100%',
-      validOn: 'January 2021'
-    }
-  ];
 
   /* ------------------------------------
   => Setter and Getter
@@ -148,10 +39,15 @@ export default class Home extends Vue {
   letter-spacing: inherit;
 }
 .banner {
-  background-image: url(https://picsum.photos/1920/1080?random)
-  background-size: cover;
+  // background-image: url(https://picsum.photos/1920/1080?random)
+  background-image: url(https://as1.ftcdn.net/jpg/02/05/42/88/500_F_205428815_0IpiDDuS65FMqnLbigzoXmjUUDpDhZVX.jpg)
+  background-repeat: repeat;
+  background-size: 10%;
+  opacity: 0.3;
   width: 100%
-  height: 600px;
+  top: 0;
+  bottom: 0;
+  position: absolute;
 }
 .slider-content {
   overflow: auto;
