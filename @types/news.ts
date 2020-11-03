cd6c4553c64a0151d02b1a55df376fb0d319cca7
trastanechora <trastanechora@gmail.com>
2020-11-03 11:29:05 +0700

    Removed unused files

--- @types/news.ts
+++
@@ -1,6 +0,0 @@
-export interface NewsItem {
-  color: string;
-  src: string;
-  title: string;
-  description: string;
-}
--- @types/promo.ts
+++
@@ -1,7 +0,0 @@
-export interface PromoItem {
-  color: string;
-  src: string;
-  title: string;
-  amount: string;
-  validOn: string;
-}
